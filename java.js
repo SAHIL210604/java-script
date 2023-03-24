e012c574590e0b1f3c7302a3df61fb4311dafbf9
Sahilkumar <sahilk3034@gmail.com>
2023-03-24 16:08:07 +0530

    animation

---
+++ java.js
@@ -0,0 +1,41 @@
+/*let x = document.getElementById("circle")
+
+x.innerText = 1
+console.log(9798527346 )
+let name ="sahil"
+console.log(name)
+let ans =false
+console.log(ans)
+ let person={
+    name:"SAHIL KUMAR",
+    Branch:"civil",
+    Age:19,
+ }
+ console.log(person.name)
+ console.log(person.Age)
+ console.log(person.Branch) 
+ console.log(person)*/
+
+ /*let x="Sahil"
+ let y="kumar"
+ console.log(x+y)*/
+
+ let x=document.getElementById("circle")
+ let sum =0
+ let p
+ 
+ function increase(){
+    let p =Number(x.innerText);
+    p +=1;
+    x.innerText =p;
+ }
+ let t =document.getElementById("s")
+ btn2.addEventListener("click",function(){
+   sum += p
+   t.textContent += "," + x.textContent
+   x.innerText =0
+   console.log(sum)
+ })
+ /*let x ="5"
+ let y = 6
+ console.log(Number(x)+y)*/
